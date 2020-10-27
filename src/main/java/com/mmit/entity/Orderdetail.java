00ac32d33e011f4e40248241270b7b1af8d34d86
Khin Su Su Han <71438900+KhinSuSuHan@users.noreply.github.com>
2020-10-27 15:24:26 +0630

    Update Orderdetail.java

--- src/main/java/com/mmit/entity/Orderdetail.java
+++ src/main/java/com/mmit/entity/Orderdetail.java
@@ -5,14 +5,8 @@ import javax.persistence.*;
 import static javax.persistence.FetchType.EAGER;
 import static javax.persistence.FetchType.LAZY;
 
-/**
- * Entity implementation class for Entity: Orderdetail
- *
- */
 @Entity
-
 public class Orderdetail implements Serializable {
-
 	
 	private static final long serialVersionUID = 1L;
 
@@ -24,49 +18,39 @@ public class Orderdetail implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "item_id")
 	private Item item;
-	
-	
-
+		
 	@ManyToOne
 	@JoinColumn(name = "order_id")
 	private Orders order;
-	
-	
+		
 	public int getId() {
 		return id;
 	}
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public int getSubQty() {
 		return subQty;
 	}
-
 
 	public void setSubQty(int subQty) {
 		this.subQty = subQty;
 	}
 
-
 	public int getSubPrice() {
 		return subPrice;
 	}
-
 
 	public void setSubPrice(int subPrice) {
 		this.subPrice = subPrice;
 	}
 
-
 	public Item getItem() {
 		return item;
 	}
-
-
+	
 	public void setItem(Item item) {
 		this.item = item;
 	}
