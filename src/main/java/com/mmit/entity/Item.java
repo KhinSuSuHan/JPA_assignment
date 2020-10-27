921cebc632b9c6c394abdb6a0486d25bace9cd3c
Khin Su Su Han <71438900+KhinSuSuHan@users.noreply.github.com>
2020-10-27 15:24:59 +0630

    Update Item.java

--- src/main/java/com/mmit/entity/Item.java
+++ src/main/java/com/mmit/entity/Item.java
@@ -5,15 +5,9 @@ import java.util.List;
 
 import javax.persistence.*;
 
-/**
- * Entity implementation class for Entity: Item
- *
- */
 @Entity
-
 public class Item implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
 
 	@Id
